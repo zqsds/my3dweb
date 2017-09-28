29a669c91de6854accba3f48dc54fb5c9bab5643
xiaofeng <xiao_feng@hunliji.com>
2017-09-28 15:17:41 +0800

    add

--- demo/qiushibaike.php
+++ demo/qiushibaike.php
@@ -51,7 +51,7 @@ $configs = array(
         'host'  => '127.0.0.1',
         'port'  => 3306,
         'user'  => 'root',
-        'pass'  => '125911',
+        'pass'  => 'root',
         'name'  => 'qiushibaike',
     ),
     //'queue_config' => array(
